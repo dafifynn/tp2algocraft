42ae53748961951cb5ea738c41daef290f7ae61e
dafi <32344462+dafifynn@users.noreply.github.com>
2019-06-05 16:08:20 -0300

    Lineal: se agrega constructor y metodo

--- src/modelo/EstrategiaDeDurabilidad/Lineal.java
+++ src/modelo/EstrategiaDeDurabilidad/Lineal.java
@@ -2,4 +2,17 @@ package modelo.EstrategiaDeDurabilidad;
 
 public class Lineal extends EstrategiaDeDurabilidad {
 
+    // Constructor
+    public Lineal(double total, double constanteDeDesgaste) {
+
+        super(total, constanteDeDesgaste);
+    }
+
+
+    // Metodos
+    public void desgastar(){
+
+        this.total -= this.constanteDeDesgaste;
+    }
+  
 }
