fbab0fca4949d2f3e94c4945bfcaffb1e9805d44
gersoto3110 <gersoto3110@gmail.com>
2019-06-05 15:43:33 -0300

    Test: hacha de piedra verifica cración
    Este test se encarga de verificar la creación de una hacha de piedra
    por defecto.

--- test/modelo/TestHachaDePiedra.java
+++ test/modelo/TestHachaDePiedra.java
@@ -8,5 +8,15 @@ import modelo.Herramienta.Hacha;
 import modelo.Jugador.Jugador;
 
 class TestHachaDePiedra extends TestCase {
-    
+
+    // Tests de integracion
+    @Test
+    void seCreaHachaDePiedraPorDefecto() {
+
+        Jugador jugador = new Jugador();
+        Hacha hachaDePiedra = jugador.crearHachaDePiedra();
+
+        assertEquals(hachaDePiedra.obtenerDurabilidad(), Constantes.DURABILIDAD_INICIAL_HACHA_PIEDRA);
+        assertEquals(hachaDePiedra.obtenerFuerza(), Constantes.FUERZA_INICIAL_HACHA_PIEDRA);
+    }
 }
