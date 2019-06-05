dcb7cc69de2cb762b81e58094b32aafab34025af
gersoto3110 <38931066+gersoto3110@users.noreply.github.com>
2019-06-05 13:16:22 -0300

    Test hacha de madera: verifica creación
    Se crea un test que se encarga de verificar la creación del hacha de madera por defecto.

--- test/modelo/TestHachaDeMadera.java
+++ test/modelo/TestHachaDeMadera.java
@@ -8,5 +8,15 @@ import modelo.Herramienta.Hacha;
 import modelo.Jugador.Jugador;
 
 class TestHachaDeMadera extends TestCase {
+    
+    // Tests de integracion
+    @Test
+    public void seCreaHachaDeMaderaPorDefecto() {
 
+        Jugador jugador = new Jugador();
+        Hacha hachaDeMadera = jugador.crearHachaDeMadera();
+
+        assertEquals(hachaDeMadera.obtenerFuerza(), Constantes.FUERZA_INICIAL_HACHA_MADERA);
+        assertEquals(hachaDeMadera.obtenerDurabilidad(), Constantes.DURABILIDAD_INICIAL_HACHA_MADERA);
+    }
 }
