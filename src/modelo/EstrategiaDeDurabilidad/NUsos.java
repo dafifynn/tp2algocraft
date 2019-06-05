3ca9b773ab314a783d66e649ed0ced3791cf60a7
dafi <32344462+dafifynn@users.noreply.github.com>
2019-06-05 16:41:37 -0300

    NUsos: se agrega constructor

--- src/modelo/EstrategiaDeDurabilidad/NUsos.java
+++ src/modelo/EstrategiaDeDurabilidad/NUsos.java
@@ -2,4 +2,13 @@ package modelo.EstrategiaDeDurabilidad;
 
 public class NUsos extends EstrategiaDeDurabilidad {
 
+    // Atributos
+    private double cantDeUsos;
+
+    // Constructor
+    public NUsos (double total, double constanteDeDesgaste, double cantDeUsos) {
+        super(total, constanteDeDesgaste);
+        this.cantDeUsos = cantDeUsos;
+    }
+  
 }
