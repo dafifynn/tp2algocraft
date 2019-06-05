2d9c386e82f0232e48733f7d47166e078087a6ec
dafi <32344462+dafifynn@users.noreply.github.com>
2019-06-05 16:14:34 -0300

    Dinamica: se agrego constructor y metodo

--- src/modelo/EstrategiaDeDurabilidad/Dinamica.java
+++ src/modelo/EstrategiaDeDurabilidad/Dinamica.java
@@ -2,4 +2,17 @@ package modelo.EstrategiaDeDurabilidad;
 
 public class Dinamica extends EstrategiaDeDurabilidad {
 
+    // Constructor
+    public Dinamica(double total, double constanteDeDesgaste) {
+
+        super(total, constanteDeDesgaste);
+    }
+
+
+    // Metodos
+    public void desgastar() {
+
+        this.total -= (this.constanteDeDesgaste * this.total);
+    }
+  
 }
