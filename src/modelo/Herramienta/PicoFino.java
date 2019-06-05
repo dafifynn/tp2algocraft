5bc91429589b5e78befe7b555e25ffacb204546c
navila96 <51213557+navila96@users.noreply.github.com>
2019-06-05 15:43:03 -0300

    PicoFino: se agrego Metodos

--- src/modelo/Herramienta/PicoFino.java
+++ src/modelo/Herramienta/PicoFino.java
@@ -23,5 +23,29 @@ public class PicoFino extends Herramienta {
         this.bloquesQuePuedoDañar = new ArrayList<Class>(
                 Arrays.asList(BloquePiedra.class, BloqueMetal.class, BloqueDiamante.class));
     }
+  
+  
+    // Metodos
+    public boolean esBloqueRompible(Bloque bloque) {
+
+        return this.bloquesQuePuedoDañar.contains(bloque.getClass());
+    }
+
+    private boolean meAfectaImpacto(Bloque bloque) {
+
+        return (bloque.getClass().isAssignableFrom(BloqueDiamante.class));
+    }
+
+    @Override
+    public void impactar(Bloque bloque){
+
+        if (this.esBloqueRompible(bloque)){
+            bloque.esGolpeado(this.fuerza);
+        }
+
+        if(this.meAfectaImpacto(bloque)){
+            this.estrategiaDeDurabilidad.desgastar();
+        }
+    }
 
 }
