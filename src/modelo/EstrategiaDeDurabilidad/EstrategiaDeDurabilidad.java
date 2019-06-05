162175030340e3dc4cdb750429572b72614ccd54
dafi <32344462+dafifynn@users.noreply.github.com>
2019-06-05 16:05:11 -0300

    EstrategiaDeDurabilidad: se agrego constructor

--- src/modelo/EstrategiaDeDurabilidad/EstrategiaDeDurabilidad.java
+++ src/modelo/EstrategiaDeDurabilidad/EstrategiaDeDurabilidad.java
@@ -1,5 +1,17 @@
 package modelo.EstrategiaDeDurabilidad;
 
 public abstract class EstrategiaDeDurabilidad {
+  
+    // Atributos
+    protected double total;
+    protected double constanteDeDesgaste;
+
+
+    // Constructor
+    public EstrategiaDeDurabilidad(double total, double constanteDeDesgaste) {
+
+        this.total = total;
+        this.constanteDeDesgaste = constanteDeDesgaste;
+    }
 
 }
