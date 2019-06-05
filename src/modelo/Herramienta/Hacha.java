eafc9a5dadabb7f5384595ff8a21bab0113f0936
navila96 <51213557+navila96@users.noreply.github.com>
2019-06-05 15:18:21 -0300

    Hacha: agrego constructor y metodos

--- src/modelo/Herramienta/Hacha.java
+++ src/modelo/Herramienta/Hacha.java
@@ -5,5 +5,17 @@ import modelo.Bloque.BloqueMadera;
 import modelo.EstrategiaDeDurabilidad.EstrategiaDeDurabilidad;
 
 public class Hacha extends Herramienta {
+  
+    // Constructor
+    public Hacha(double fuerza, EstrategiaDeDurabilidad estrategia) {
+        super(fuerza, estrategia);
+    }
+    
+    
+    // Metodos
+    public boolean esBloqueRompible(Bloque bloque){
+
+        return (bloque.getClass().isAssignableFrom(BloqueMadera.class));
+    }
 
 }
