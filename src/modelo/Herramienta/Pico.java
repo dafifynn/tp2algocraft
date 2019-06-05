a6301d925b2c6ddbbddf679a31a9bc81a2953399
navila96 <51213557+navila96@users.noreply.github.com>
2019-06-05 15:22:12 -0300

    Pico: agrego constructor y metodos

--- src/modelo/Herramienta/Pico.java
+++ src/modelo/Herramienta/Pico.java
@@ -6,5 +6,18 @@ import modelo.Bloque.BloquePiedra;
 import modelo.EstrategiaDeDurabilidad.EstrategiaDeDurabilidad;
 
 public class Pico extends Herramienta {
+  
+    // Constructor
+    public Pico(double fuerza, EstrategiaDeDurabilidad estrategia) {
+        super(fuerza, estrategia);
+    }
+
+
+    // Metodos
+    public boolean esBloqueRompible(Bloque bloque){
+
+        return(bloque.getClass().isAssignableFrom(BloquePiedra.class) || bloque.getClass().isAssignableFrom(BloqueMetal.class));
+
+    }
 
 }
