f9e0f3dcd722344fd39429a1086407870ade1c19
gersoto3110 <gersoto3110@gmail.com>
2019-06-05 15:47:58 -0300

    Test: hacha de piedra impacta con un bloque madera
    Este test verifica que el hacha de piedra se desgasta de forma correcta
    cuando impacta con un bloque de madera.

--- test/modelo/TestHachaDePiedra.java
+++ test/modelo/TestHachaDePiedra.java
@@ -19,4 +19,17 @@ class TestHachaDePiedra extends TestCase {
         assertEquals(hachaDePiedra.obtenerDurabilidad(), Constantes.DURABILIDAD_INICIAL_HACHA_PIEDRA);
         assertEquals(hachaDePiedra.obtenerFuerza(), Constantes.FUERZA_INICIAL_HACHA_PIEDRA);
     }
+
+    @Test
+    void hachaDePiedraSeUsaContraMaderaYSeReduceSuDurabilidadDeFormaCorrespondiente(){
+
+        Jugador jugador = new Jugador();
+        Hacha hachaDePiedra = jugador.crearHachaDePiedra();
+        BloqueMadera bloqueDeMadera = new BloqueMadera();
+
+        hachaDePiedra.impactar(bloqueDeMadera);
+
+        assertEquals(hachaDePiedra.obtenerDurabilidad(),Constantes.DURABILIDAD_INICIAL_HACHA_PIEDRA -
+                Constantes.FUERZA_INICIAL_HACHA_PIEDRA );
+    }
 }
