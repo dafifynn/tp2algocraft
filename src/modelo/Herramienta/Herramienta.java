05e6810b15a846f4a3ea7d2316cec8d87999f58b
navila96 <51213557+navila96@users.noreply.github.com>
2019-06-05 15:06:33 -0300

    Herramienta: se agrego constructor y getters

--- src/modelo/Herramienta/Herramienta.java
+++ src/modelo/Herramienta/Herramienta.java
@@ -4,5 +4,30 @@ import modelo.Bloque.Bloque;
 import modelo.EstrategiaDeDurabilidad.EstrategiaDeDurabilidad;
 
 public abstract class Herramienta{
+  
+    // Atributos
+    protected double fuerza;
+
+    protected EstrategiaDeDurabilidad estrategiaDeDurabilidad;
+
+
+    // Constructor
+    public Herramienta(double fuerza, EstrategiaDeDurabilidad estrategia) {
+
+        this.fuerza = fuerza;
+        this.estrategiaDeDurabilidad = estrategia;
+    }
+
+
+    // Getters
+    public double obtenerFuerza() {
+
+        return this.fuerza;
+    }
+
+    public double obtenerDurabilidad() {
+
+        return this.estrategiaDeDurabilidad.obtenerValorActual();
+    }
 
 }
