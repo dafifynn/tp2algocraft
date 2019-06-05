38ae60b827bf4bcfd8140859dbbc602f67210ccc
navila96 <51213557+navila96@users.noreply.github.com>
2019-06-05 15:15:09 -0300

    Hacha: creo el archivo Hacha

---
+++ src/modelo/Herramienta/Hacha.java
@@ -0,0 +1,9 @@
+package modelo.Herramienta;
+
+import modelo.Bloque.Bloque;
+import modelo.Bloque.BloqueMadera;
+import modelo.EstrategiaDeDurabilidad.EstrategiaDeDurabilidad;
+
+public class Hacha extends Herramienta {
+
+}
