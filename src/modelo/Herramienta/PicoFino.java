507c7ff88c92ca06d80ecb712c8856764b38fea4
navila96 <51213557+navila96@users.noreply.github.com>
2019-06-05 15:41:31 -0300

    PicoFino: se crea el archivo PicoFino

---
+++ src/modelo/Herramienta/PicoFino.java
@@ -0,0 +1,15 @@
+package modelo.Herramienta;
+
+import modelo.Bloque.Bloque;
+import modelo.Bloque.BloqueDiamante;
+import modelo.Bloque.BloqueMetal;
+import modelo.Bloque.BloquePiedra;
+import modelo.EstrategiaDeDurabilidad.EstrategiaDeDurabilidad;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class PicoFino extends Herramienta {
+
+}
