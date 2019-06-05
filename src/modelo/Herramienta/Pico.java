9fcc886061baef60b99227ffcfb29a5cf2684e21
navila96 <51213557+navila96@users.noreply.github.com>
2019-06-05 15:20:49 -0300

    Pico: agrefo el archivo java

---
+++ src/modelo/Herramienta/Pico.java
@@ -0,0 +1,10 @@
+package modelo.Herramienta;
+
+import modelo.Bloque.Bloque;
+import modelo.Bloque.BloqueMetal;
+import modelo.Bloque.BloquePiedra;
+import modelo.EstrategiaDeDurabilidad.EstrategiaDeDurabilidad;
+
+public class Pico extends Herramienta {
+
+}
