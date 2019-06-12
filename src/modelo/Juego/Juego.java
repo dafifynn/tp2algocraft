c2d4e5024512c346a86fbb76b1bd666281f65a43
Nelson <navila@fi.uba.ar>
2019-06-12 01:31:29 -0300

    Juego: cambios por la creacion de Mapa

--- src/modelo/Juego/Juego.java
+++ src/modelo/Juego/Juego.java
@@ -1,56 +1,22 @@
 package modelo.Juego;
 
-import modelo.Constantes;
-import modelo.Grilla.Grilla;
 import modelo.Jugador.Jugador;
+import modelo.Mapa.Mapa;
+import modelo.Material.Material;
+
 
 public class Juego {
 
-    private Grilla<Object> mapa;
+    // Atributos
+    private Mapa<Material> mapa;
+    private Jugador jugador;
 
-    private int xJugador;
 
-    private int yJugador;
-
+    // Constructor
     public Juego() {
-        this.mapa = new Grilla(Constantes.GRILLA_FILAS_DEFECTO, Constantes.GRILLA_COLUMNAS_DEFECTO);
-        this.crearJugador();
+        this.mapa = new Mapa<>();
+        this.jugador = new Jugador();
+
     }
 
-    public Juego(int x,int y) {
-        this.mapa = new Grilla(x,y);
-    }
-
-    private void crearJugador() {
-
-        Jugador jugador = new Jugador();
-        xJugador = Constantes.JUGADOR_POSICION_X_DEFECTO;
-        yJugador = Constantes.JUGADOR_POSICION_Y_DEFECTO;
-
-        mapa.agregar(jugador,xJugador,yJugador);
-    }
-
-//    public void moverJugador(String direccion) {
-//
-//        int xActualizada = this.xJugador;
-//        int yActualizada = this.xJugador;
-//
-//        switch (direccion) {
-//            case Constantes.NORTE: yActualizada--;
-//            break;
-//            case Constantes.SUR: yActualizada++;
-//            break;
-//            case Constantes.OESTE: xActualizada--;
-//            break;
-//            case Constantes.ESTE: xActualizada++;
-//        }
-//
-//        if(mapa.desplazar(this.xJugador, this.yJugador, xActualizada, yActualizada)) {
-//            this.xJugador = xActualizada;
-//        }
-//    }
-
-    public void dibujar() {
-        Artista.dibujarMundo(this.mapa);
-    }
 }
