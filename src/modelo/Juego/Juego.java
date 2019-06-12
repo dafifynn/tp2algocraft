f926f697c8db90865064ec4ec4dcea23b25adb46
daphne <dhernandez@fi.uba.ar>
2019-06-12 20:37:43 -0300

    juego: se actualizo Juego
    por nueva implementacion de Mapa

--- src/modelo/Juego/Juego.java
+++ src/modelo/Juego/Juego.java
@@ -2,19 +2,18 @@ package modelo.Juego;
 
 import modelo.Jugador.Jugador;
 import modelo.Mapa.Mapa;
-import modelo.Material.Material;
 
 
 public class Juego {
 
     // Atributos
-    private Mapa<Material> mapa;
+    private Mapa  mapa;
     private Jugador jugador;
 
 
     // Constructor
     public Juego() {
-        this.mapa = new Mapa<>();
+        this.mapa = new Mapa();
         this.jugador = new Jugador();
 
     }
