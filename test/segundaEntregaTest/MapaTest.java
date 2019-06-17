3b05b75bd88947671380995bba6494ecb5cb4dbc
gersoto3110 <gersoto3110@gmail.com>
2019-06-17 19:58:14 -0300

    Test: Se crea test de mapa

---
+++ test/segundaEntregaTest/MapaTest.java
@@ -0,0 +1,51 @@
+package segundaEntregaTest;
+
+import modelo.Excepciones.CoordenadaOcupadaException;
+import modelo.Mapa.Coordenada;
+import modelo.Mapa.Mapa;
+import modelo.Material.Material;
+import modelo.Material.MaterialMadera;
+import modelo.Material.MaterialMetal;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class MapaTest {
+    @Test
+    public void colocarUnMaterialAlMapaVacioInicialmenteEnLaCoordenadaX0Y1 () {
+        Material madera = new MaterialMadera();
+
+        // Se verifica que esa coordenada esta vacia.
+        Mapa mapa = new Mapa();
+        Coordenada coordenada = new Coordenada(0,1);
+
+        assertEquals(false, mapa.hayMaterialEnCoordenada(coordenada));
+
+        // Se agrega el material al mapa.
+        mapa.agregarMaterial(coordenada, madera);
+        assertEquals(true, mapa.hayMaterialEnCoordenada(coordenada));
+    }
+
+    @Test
+    public void colocarMaterialDeMaderaEnLaMismaCoordenadaRetornaCoordenadaOcupadaException () {
+        Mapa mapa = new Mapa();
+        Coordenada coordenada = new Coordenada(0,1);
+
+        // Se agrega el material al mapa.
+        mapa.agregarMaterial(coordenada, new MaterialMadera());
+
+        assertThrows(CoordenadaOcupadaException.class, () -> {mapa.agregarMaterial(coordenada, new MaterialMadera());});
+    }
+
+    @Test
+    public void colocarMaterialDistintoEnLaMismaCoordenadaRetornaCoordenadaOcupadaException () {
+        Mapa mapa = new Mapa();
+        Coordenada coordenada = new Coordenada(0,1);
+
+        // Se agrega el material al mapa.
+        mapa.agregarMaterial(coordenada, new MaterialMadera());
+
+        assertThrows(CoordenadaOcupadaException.class, () -> {mapa.agregarMaterial(coordenada, new MaterialMetal());});
+    }
+}
