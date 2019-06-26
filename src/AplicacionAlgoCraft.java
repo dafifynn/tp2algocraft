6339cfaaf879859e42047109f7d8ea6c88b7f85c
daphne <dhernandez@fi.uba.ar>
2019-06-26 17:40:31 -0300

    probando push

--- src/AplicacionAlgoCraft.java
+++ src/AplicacionAlgoCraft.java
@@ -2,7 +2,6 @@ import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 
-
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 import modelo.Mapa.Mapa;
